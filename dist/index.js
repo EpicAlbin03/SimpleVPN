a8ec5c8411710536291b1cc23e9c81d87bdb03eb
EpicAlbin03 <epicalbin03@gmail.com>
2022-02-07 20:09:27 +0100

    menu dropdown

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,21 @@
 "use strict";
+const nav = document.getElementById("navbar");
 // Change background of nav to white when user scrolls for the first time
 window.addEventListener("scroll", () => {
-    const nav = document.getElementById("navbar");
     if (window.scrollY == 0) {
         nav.style.backgroundColor = "";
     }
     else {
         nav.style.backgroundColor = "white";
+    }
+});
+document.getElementById("navDropdown")?.addEventListener("click", () => {
+    if (nav.style.backgroundColor == "white") {
+        nav.style.backgroundColor = "";
+        document.body.style.overflowY = "";
+    }
+    else {
+        nav.style.backgroundColor = "white";
+        document.body.style.overflowY = "hidden";
     }
 });
