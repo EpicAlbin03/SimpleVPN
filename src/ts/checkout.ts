5a2510624530bb1094da4371722536bb29a9d241
EpicAlbin03 <epicalbin03@gmail.com>
2022-02-10 12:04:34 +0100

    continued checkout form

---
+++ src/ts/checkout.ts
@@ -0,0 +1,12 @@
+const chevron = document.getElementsByClassName("bi");
+const dropdown = document.getElementsByClassName("dropdown");
+
+for (let i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("show.bs.dropdown", function () {
+    chevron[i].className = "bi bi-chevron-down float-end";
+  });
+
+  dropdown[i].addEventListener("hide.bs.dropdown", function () {
+    chevron[i].className = "bi bi-chevron-right float-end";
+  });
+}
