acce1ab609e0db2323b1164fea87c48b55175010
EpicAlbin03 <epicalbin03@gmail.com>
2022-02-10 20:32:28 +0100

    small update

--- src/ts/checkout.ts
+++ src/ts/checkout.ts
@@ -3,10 +3,10 @@ const dropdown = document.getElementsByClassName("dropdown");
 
 for (let i = 0; i < dropdown.length; i++) {
   dropdown[i].addEventListener("show.bs.dropdown", function () {
-    chevron[i].className = "bi bi-chevron-down float-end";
+    chevron[i].className = "bi bi-chevron-down float-end fs-5";
   });
 
   dropdown[i].addEventListener("hide.bs.dropdown", function () {
-    chevron[i].className = "bi bi-chevron-right float-end";
+    chevron[i].className = "bi bi-chevron-right float-end fs-5";
   });
 }
