89f777b236f93bb88e0c707920e62a9507c0dbc1
EpicAlbin03 <epicalbin03@gmail.com>
2022-02-24 14:23:10 +0100

    blog + scrollbar

---
+++ dist/js/blog.js
@@ -0,0 +1,20 @@
+"use strict";
+const cardGroup = document.getElementsByClassName("cardGroup");
+function myFunction(x) {
+    if (x.matches) {
+        // If media query matches
+        for (let i = 0; i < cardGroup.length; i++) {
+            cardGroup[i].classList.remove("row-cols-2");
+            cardGroup[i].classList.add("row-cols-1");
+        }
+    }
+    else {
+        for (let i = 0; i < cardGroup.length; i++) {
+            cardGroup[i].classList.add("row-cols-2");
+            cardGroup[i].classList.remove("row-cols-1");
+        }
+    }
+}
+var x = window.matchMedia("(max-width: 310px)");
+myFunction(x); // Call listener function at run time
+x.addListener(myFunction); // Attach listener function on state changes
